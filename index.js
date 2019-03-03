a16914e8ad3410a71bc16f2e81c6189126355166
David Bolin <david@bolin.at>
2019-03-03 15:37:27 -0800

    finished html and css

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const store = {
+  meals: 0,
+  tips: 0,
+  price: 0,
+  tax: 0,
+  percentage: 0
+};
+
+function render() {
+
+}
+
+function handleInput() {
+
+}
+
+function handleReset() {
+
+}
+
+
+function main() {
+  handleInput();
+  handleReset();
+  render();
+}
+
+$(main);
